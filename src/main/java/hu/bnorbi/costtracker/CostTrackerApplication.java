e104b5e90a2dad45fab1d8a00ccc7c32f253257e
Norbert Balogh <norbert.balogh@ad4est.com>
2022-05-22 19:01:36 +0200

    Initial comment

---
+++ src/main/java/hu/bnorbi/costtracker/CostTrackerApplication.java
@@ -0,0 +1,13 @@
+package hu.bnorbi.costtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostTrackerApplication.class, args);
+	}
+
+}
